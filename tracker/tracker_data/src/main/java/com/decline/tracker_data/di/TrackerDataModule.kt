511a0fc45718d41dbe1d2f19467a35bb6fe7e9e9
calvin <calvindc96@gmail.com>
2022-02-01 22:11:59 +0100

    Setup DB and created a DAO

--- tracker/tracker_data/src/main/java/com/decline/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/decline/tracker_data/di/TrackerDataModule.kt
@@ -1,5 +1,8 @@
 package com.decline.tracker_data.di
 
+import android.app.Application
+import androidx.room.Room
+import com.decline.tracker_data.local.TrackerDatabase
 import com.decline.tracker_data.remote.OpenFoodApi
 import com.decline.tracker_data.remote.OpenFoodApi.Companion.BASE_URL
 import dagger.Module
@@ -38,5 +41,15 @@ object TrackerDataModule {
             .client(client)
             .build()
             .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerDatabase(application: Application): TrackerDatabase {
+        return Room.databaseBuilder(
+            application,
+            TrackerDatabase::class.java,
+            "tracker_db"
+        ).build()
     }
 }
