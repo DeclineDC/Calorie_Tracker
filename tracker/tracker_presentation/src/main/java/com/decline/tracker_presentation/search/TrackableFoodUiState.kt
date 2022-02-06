725c7b12c856a7a508f5f03932c5346b850d4016
calvin <calvindc96@gmail.com>
2022-02-06 13:45:22 +0100

    Added SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/decline/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.decline.tracker_presentation.search
+
+import com.decline.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
